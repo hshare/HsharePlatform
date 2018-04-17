bd09efcbbe49acb5df10ef1ca649dbc5fac1fbc3
hcare <1015918929@qq.com>
2018-04-17 22:06:51 +0800

    fff

--- p-business-module/business-module-j3new/src/main/java/hshare/business/module/j3new/J3NewActivity.java
+++ p-business-module/business-module-j3new/src/main/java/hshare/business/module/j3new/J3NewActivity.java
@@ -3,11 +3,14 @@ package hshare.business.module.j3new;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import hshare.base.component.view.statusbar.StatusBarUtil;
+
 public class J3NewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gan);
+        StatusBarUtil.setTranslucentForImageView(this,110,null);
     }
 }
